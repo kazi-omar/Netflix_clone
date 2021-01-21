df7e066ba3758ff9b4922117acfa8dd84451b4ed
kazi-omar <kaziomar383@gmail.com>
2021-01-22 02:19:38 +0600

    adding starting project

---
+++ build/precache-manifest.ecb45f63c701cc1a2385d840887951f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe85b469baf67efde106fc8ec47219f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b1e478fd6d667659832",
+    "url": "/static/css/main.8039da46.chunk.css"
+  },
+  {
+    "revision": "b7c448a785f81979bbe5",
+    "url": "/static/js/2.2125339c.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/static/js/2.2125339c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b1e478fd6d667659832",
+    "url": "/static/js/main.59b144de.chunk.js"
+  },
+  {
+    "revision": "154cfa12c4362043459b",
+    "url": "/static/js/runtime-main.43d83561.js"
+  }
+]);
---
+++ src/Nav.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.addEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav_avatar"
+        src="https://www.themoviedb.org/assets/1/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png"
+        alt="Netflix Avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
